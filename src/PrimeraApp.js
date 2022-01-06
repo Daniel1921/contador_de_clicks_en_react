4c83d58edbc0cae445b48eb8e43612c69fb4547e
Daniel Mafla Bonet <danielmaflab@hotmail.com>
2022-01-05 22:07:56 -0500

    app de clicks terminada

---
+++ src/PrimeraApp.js
@@ -0,0 +1,34 @@
+// import { Fragment } from "react";
+import PropTypes from 'prop-types';
+
+// funtional component
+
+const PrimeraApp = ( {saludo, subtitulo} ) => {
+    
+    // const saludo = ' Hola Mundo en Español '
+
+
+    // const x = {
+    //     nombre: 'Daniel',
+    //     edad: 25
+    // }
+
+
+    return ( <>
+                <h1> {saludo} </h1>
+                <h3>{subtitulo}</h3>
+                {/* <pre>{JSON.stringify(x, null, 3)}</pre> */}
+             </>
+            );
+}
+
+PrimeraApp.propTypes = {
+    saludo: PropTypes.string.isRequired
+}
+
+PrimeraApp.defaultProps = {
+    subtitulo: 'Componentes y propiedades en React'
+}
+
+
+export default PrimeraApp;
